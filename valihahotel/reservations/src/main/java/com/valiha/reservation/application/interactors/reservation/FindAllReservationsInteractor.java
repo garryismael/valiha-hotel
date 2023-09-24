c8fb246b6209b453483f9229d24834d678a52337
Garry Ismaël <tahinjanaharygarry@gmail.com>
2023-09-24 13:00:53 +0300

    setup READ of reservation

--- valihahotel/reservations/src/main/java/com/valiha/reservation/application/interactors/reservation/FindAllReservationsInteractor.java
+++ valihahotel/reservations/src/main/java/com/valiha/reservation/application/interactors/reservation/FindAllReservationsInteractor.java
@@ -1,15 +1,31 @@
 package com.valiha.reservation.application.interactors.reservation;
 
 import com.valiha.reservation.application.dto.reservation.ReservationResponseDto;
+import com.valiha.reservation.application.presenter.GenericPresenter;
+import com.valiha.reservation.application.repository.ReservationRepository;
 import com.valiha.reservation.application.useCase.reservation.FindAllReservationsUseCase;
+import com.valiha.reservation.core.entities.models.Reservation;
 import java.util.List;
 
 public class FindAllReservationsInteractor
   implements FindAllReservationsUseCase {
 
+  private final ReservationRepository reservationRepository;
+  private final GenericPresenter<ReservationResponseDto> reservationPresenter;
+
+  public FindAllReservationsInteractor(
+    ReservationRepository reservationRepository,
+    GenericPresenter<ReservationResponseDto> reservationPresenter
+  ) {
+    this.reservationRepository = reservationRepository;
+    this.reservationPresenter = reservationPresenter;
+  }
+
   @Override
   public List<ReservationResponseDto> execute() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    List<Reservation> reservations = this.reservationRepository.findAll();
+    return reservationPresenter.prepareSuccessView(
+      ReservationResponseDto.from(reservations)
+    );
   }
 }
--- valihahotel/reservations/src/main/java/com/valiha/reservation/application/interactors/reservation/FindOneReservationInteractor.java
+++ valihahotel/reservations/src/main/java/com/valiha/reservation/application/interactors/reservation/FindOneReservationInteractor.java
@@ -1,13 +1,45 @@
 package com.valiha.reservation.application.interactors.reservation;
 
 import com.valiha.reservation.application.dto.reservation.ReservationResponseDto;
+import com.valiha.reservation.application.presenter.GenericPresenter;
+import com.valiha.reservation.application.repository.ReservationRepository;
 import com.valiha.reservation.application.useCase.reservation.FindOneReservationUseCase;
+import com.valiha.reservation.core.constant.ReservationValidator;
+import com.valiha.reservation.core.entities.models.Reservation;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FindOneReservationInteractor implements FindOneReservationUseCase {
 
+  private final ReservationRepository reservationRepository;
+  private final GenericPresenter<ReservationResponseDto> reservationPresenter;
+
+  public FindOneReservationInteractor(
+    ReservationRepository reservationRepository,
+    GenericPresenter<ReservationResponseDto> reservationPresenter
+  ) {
+    this.reservationRepository = reservationRepository;
+    this.reservationPresenter = reservationPresenter;
+  }
+
   @Override
   public ReservationResponseDto execute(String id) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    Reservation reservation = this.reservationRepository.findOneById(id);
+
+    if (reservation == null) {
+      Map<String, String> errors = new HashMap<String, String>();
+      errors.put(
+        ReservationValidator.KEY_ID,
+        String.format(ReservationValidator.RESERVATION_NOT_FOUND_ERROR, id)
+      );
+      return this.reservationPresenter.prepareResourceNotFoundView(
+          ReservationValidator.RESERVATION_NOT_FOUND_ERROR,
+          errors
+        );
+    }
+
+    return this.reservationPresenter.prepareSuccessView(
+        ReservationResponseDto.from(reservation)
+      );
   }
 }
