6bb4a1d3069ec5a31c9577a89d0aa0890a35efb0
Garry Ismaël <tahinjanaharygarry@gmail.com>
2023-09-25 11:20:17 +0300

    remove spring data rest

--- valihahotel/reservations/src/main/java/com/valiha/reservation/presentation/config/WebSecurity.java
+++ valihahotel/reservations/src/main/java/com/valiha/reservation/presentation/config/WebSecurity.java
@@ -1,5 +1,12 @@
 package com.valiha.reservation.presentation.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,34 +19,78 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class WebSecurity {
-    private static final String[] AUTH_WHITELIST = {
-            "/docs/**", "/api-docs/**",
-            "/uploads/**", "/static/**"
+
+  private static final String[] AUTH_WHITELIST = {
+    "/docs/**",
+    "/api-docs/**",
+    "/uploads/**",
+    "/static/**",
+  };
+
+  @Bean
+  SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+    Customizer<CsrfSpec> customizer = new Customizer<CsrfSpec>() {
+      @Override
+      public void customize(CsrfSpec t) {
+        t.disable();
+      }
     };
 
-    @Bean
-    SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
-        Customizer<CsrfSpec> customizer = new Customizer<CsrfSpec>() {
-            @Override
-            public void customize(CsrfSpec t) {
-                t.disable();
-            }
-        };
+    Customizer<CorsSpec> cors = new Customizer<CorsSpec>() {
+      @Override
+      public void customize(CorsSpec c) {
+        c.disable();
+      }
+    };
 
-        Customizer<CorsSpec> cors = new Customizer<CorsSpec>() {
-            @Override
-            public void customize(CorsSpec c) {
-                c.disable();
-            }
-        };
+    http
+      .csrf(customizer)
+      .cors(cors)
+      .authorizeExchange(exchange ->
+        exchange
+          .pathMatchers(AUTH_WHITELIST)
+          .permitAll()
+          .anyExchange()
+          .authenticated()
+      );
 
-        http.csrf(customizer)
-                .cors(cors)
-                .authorizeExchange(
-                        exchange -> exchange.pathMatchers(AUTH_WHITELIST)
-                                .permitAll().anyExchange().authenticated());
+    http.oauth2ResourceServer(
+      (oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+    );
+    return http.build();
+  }
 
-        http.oauth2ResourceServer((oauth2 -> oauth2.jwt(Customizer.withDefaults())));
-        return http.build();
-    }
+  @Bean
+  OpenAPI openAPI() {
+    return new OpenAPI()
+      .addSecurityItem(
+        new SecurityRequirement().addList("Bearer Authentication")
+      )
+      .components(
+        new Components()
+          .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme())
+      )
+      .info(
+        new Info()
+          .title("User Service API")
+          .description("Micro service of User of Valiha Hotel")
+          .version("1.0")
+          .contact(
+            new Contact()
+              .name("Garry")
+              .email("tahinjanaharygarry@gmail.com")
+              .url("tahinjanaharygarry@gmail.com")
+          )
+          .license(
+            new License().name("Owned by Valiha Hotel").url("valihahotel.com")
+          )
+      );
+  }
+
+  private SecurityScheme createAPIKeyScheme() {
+    return new SecurityScheme()
+      .type(SecurityScheme.Type.HTTP)
+      .bearerFormat("JWT")
+      .scheme("bearer");
+  }
 }
