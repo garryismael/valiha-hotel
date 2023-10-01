679ca3eb41fdc58a2df9570594c15912191c9b54
Garry Ismaël <tahinjanaharygarry@gmail.com>
2023-10-01 08:30:14 +0300

    use spring webflux for gateway

--- valihahotel/gateway/src/main/java/com/valiha/gateway/config/SecurityConfig.java
+++ valihahotel/gateway/src/main/java/com/valiha/gateway/config/SecurityConfig.java
@@ -3,22 +3,22 @@ package com.valiha.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
   @Bean
-  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  public SecurityWebFilterChain securityWebFilterChain(
+    ServerHttpSecurity http
+  ) {
     http
-      .csrf(c -> c.disable())
       .cors(c -> c.disable())
-      .authorizeHttpRequests(authz -> {
-        authz.anyRequest().authenticated();
-      });
+      .csrf(c -> c.disable())
+      .authorizeExchange(exchange -> exchange.anyExchange().authenticated());
 
     http.oauth2ResourceServer(
       (oauth2 -> oauth2.jwt(Customizer.withDefaults()))
