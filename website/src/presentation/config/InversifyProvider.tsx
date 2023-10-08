5e05ecb04888b3fa3490e05ad60c6a731fc2bb21
Garry Ismaël <tahinjanaharygarry@gmail.com>
2023-10-08 13:40:02 +0300

    remove provider

--- website/src/presentation/config/InversifyProvider.tsx
+++
@@ -1,8 +0,0 @@
-import { Provider } from "inversify-react";
-import container from "./container";
-
-const InversifyProvider = ({ children }: { children: React.ReactNode }) => {
-  return <Provider container={container}>{children}</Provider>;
-};
-
-export default InversifyProvider;
