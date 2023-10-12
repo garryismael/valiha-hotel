454170875a9b2f134568961536764b0593dede50
Garry Ismaël <tahinjanaharygarry@gmail.com>
2023-10-12 15:51:50 +0300

    permit GET method

--- valihahotel/gateway/src/main/java/com/valiha/gateway/config/SecurityConfig.java
+++ valihahotel/gateway/src/main/java/com/valiha/gateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.valiha.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -11,12 +12,23 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+  private static final String[] AUTH_API_GET = {
+    "/RESERVATIONS-SERVICE/categories",
+    "/RESERVATIONS-SERVICE/rooms",
+  };
+
   @Bean
   SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
     http
       .cors(c -> c.disable())
       .csrf(c -> c.disable())
-      .authorizeExchange(exchange -> exchange.anyExchange().authenticated());
+      .authorizeExchange(exchange ->
+        exchange
+          .pathMatchers(HttpMethod.GET, AUTH_API_GET)
+          .permitAll()
+          .anyExchange()
+          .authenticated()
+      );
 
     http.oauth2ResourceServer(
       (oauth2 -> oauth2.jwt(Customizer.withDefaults()))
