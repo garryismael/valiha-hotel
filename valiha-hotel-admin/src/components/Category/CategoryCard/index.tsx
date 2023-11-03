3a151864e74153fcbb7a6f4c5c2f270e76d873c1
Garry Ismaël <tahinjanaharygarry@gmail.com>
2023-11-03 08:01:38 +0300

    add category action

--- valiha-hotel-admin/src/components/Category/CategoryCard/index.tsx
+++ valiha-hotel-admin/src/components/Category/CategoryCard/index.tsx
@@ -4,6 +4,14 @@ import Link from "next/link";
 import { BiBath } from "react-icons/bi";
 import { MdOutlineBed } from "react-icons/md";
 import styles from "./index.module.css";
+import { FaEllipsisVertical } from "react-icons/fa6";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react";
 
 export default function CategoryCard({ category }: { category: Category }) {
   return (
@@ -16,6 +24,30 @@ export default function CategoryCard({ category }: { category: Category }) {
           sizes="100%"
           className={styles.image}
         />
+        <div className={styles.actions}>
+          <Dropdown
+            radius="sm"
+            className="bg-background border-1 border-default-200"
+          >
+            <DropdownTrigger className="outline-none">
+              <Button
+                isIconOnly
+                radius="none"
+                className="rounded-bl-3xl outline-none"
+                size="lg"
+                variant="light"
+              >
+                <FaEllipsisVertical size={18} className="text-dark-400" />
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu>
+              <DropdownItem>Detail</DropdownItem>
+              <DropdownItem>Bloquer</DropdownItem>
+              <DropdownItem>Modifier</DropdownItem>
+              <DropdownItem>Supprimer</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
       </div>
       <div className="pb-6 mt-5 ml-2">
         <Link href="#" className={styles.room__category}>
