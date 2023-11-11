5f2a3aa5426b639596872b2f5fcc19e63e20a75e
Garry Ismaël <tahinjanaharygarry@gmail.com>
2023-11-11 15:41:49 +0300

    change importer annotation

--- valihahotel/reservations/src/main/java/com/valiha/reservation/presentation/controller/ShuttleController.java
+++ valihahotel/reservations/src/main/java/com/valiha/reservation/presentation/controller/ShuttleController.java
@@ -4,11 +4,11 @@ import com.valiha.reservation.application.dto.shuttle.ShuttleRequestDto;
 import com.valiha.reservation.application.dto.shuttle.ShuttleResponseDto;
 import com.valiha.reservation.application.useCase.shuttle.DeleteShuttleUseCase;
 import com.valiha.reservation.application.useCase.shuttle.EditShuttleUseCase;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
