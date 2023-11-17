1c302f315ae093e233eeabe1cc44428784709387
Garry Ismaël <tahinjanaharygarry@gmail.com>
2023-11-17 10:41:31 +0300

    update files

--- valihahotel/notifications/src/main/java/com/valiha/notification/infrastructures/mail/LocationServiceImpl.java
+++ valihahotel/notifications/src/main/java/com/valiha/notification/infrastructures/mail/LocationServiceImpl.java
@@ -13,8 +13,10 @@ import java.util.Map;
 import lombok.AllArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
+@Service
 @AllArgsConstructor
 public class LocationServiceImpl implements LocationService {
 
